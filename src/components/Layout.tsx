5cf1ecf0e3b82320f61555ca2e735ee4287041b4
Felipe Duitama <felipe.duitama@3dves.com>
2021-03-09 00:10:08 -0500

    refactor layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,32 +11,6 @@ const Layout = ({ children, title = "LOD - Ckan analyzer" }: Props) => (
   <div>
     <Head>
       <title>LOD - {title}</title>
-      <meta charSet="utf-8" />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, shrink-to-fit=no"
-      />
-      <meta name="theme-color" content="#000000" />
-
-      <link rel="shortcut icon" href="/favicon.ico" />
-      <link
-        rel="stylesheet"
-        href="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.css"
-      />
-
-      <script src="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"
-      />
-      <link
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        rel="stylesheet"
-      />
-      <link
-        rel="stylesheet"
-        href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-      />
     </Head>
     <header>
       <nav>
