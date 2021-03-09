b9a7febee3c0842c9ce02c5ecb270a9bd3ceb704
Felipe Duitama <felipe.duitama@3dves.com>
2021-03-08 23:31:49 -0500

    add error styles

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,17 +1,19 @@
 import { Box, Typography } from "@material-ui/core";
 import React from "react";
+import useStyles from "./error.styles";
 
 type Props = {
   error: CustomError;
 };
 
 const Error = ({ error }: Props) => {
+  const classes = useStyles();
   const code = error.code.toString();
   return (
-    <Box>
+    <Box className={classes.root}>
       <Typography variant="h3">{code}</Typography>
       <Typography variant="body2">{error.message}</Typography>
-      <img src={error.image} alt={code} />
+      <img src={`/static/images/errorCodes${error.image}`} alt={code} />
     </Box>
   );
 };
