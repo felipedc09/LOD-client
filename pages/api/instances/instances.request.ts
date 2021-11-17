0f5f91dcd882828e058ae6f52a9d51e328a1768a
Felipe Duitama <felipedc09@gmail.com>
2021-11-17 17:54:05 -0500

    UPDATE: fix styles

--- pages/api/instances/instances.request.ts
+++ pages/api/instances/instances.request.ts
@@ -1,22 +1,22 @@
 import errorHandler from "@/utils/errorHandler";
-import  axios  from "axios";
+import axios from "axios";
 
-const baseURL = process.env.BASE_URL
+const baseURL = process.env.BASE_URL;
 
 export const getInstances = async (): Promise<Instance[]> => {
-    try {
-       const {data: instances} = await axios.get(`${baseURL}/instances`)
-       return instances
-    } catch (error) {
-      throw errorHandler(error)  
-    }
-}
+  try {
+    const { data: instances } = await axios.get(`${baseURL}/instances`);
+    return instances;
+  } catch (error) {
+    throw errorHandler(error);
+  }
+};
 
 export const getInstanceById = async (id: string): Promise<Instance[]> => {
-    try {
-       const {data: instance} = await axios.get(`${baseURL}/instances/${id}`)
-       return instance
-      } catch (error) {
-      throw errorHandler(error)  
-    }
-}
+  try {
+    const { data: instance } = await axios.get(`${baseURL}/instances/${id}`);
+    return instance;
+  } catch (error) {
+    throw errorHandler(error);
+  }
+};
